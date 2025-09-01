0c6d93b9a70d886ae9c897f55041ca67e33f0dab
theadesolasarah <williamssarahadesola@gmail.com>
2025-09-01 16:22:30 +0100

    add UserProfile component and integrate it in App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -28,6 +29,14 @@ function App() {
       <MainContent />
 
       <Footer />
+
+      <div>
+        <UserProfile
+          name="Sarah"
+          age="23"
+          bio="Passionate learner diving into frontend development"
+        />
+      </div>
     </>
   );
 }
