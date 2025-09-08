75eaef613abc95f2c902e1de2a8f473938701d7d
theadesolasarah <williamssarahadesola@gmail.com>
2025-09-08 18:18:24 +0100

    add inline CSS styling to all components

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import WelcomeMessage from "./components/WelcomeMessage";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+
+      <WelcomeMessage />
+
+      <Header />
+
+      <MainContent />
+
+      <Footer />
+
+      <div>
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      </div>
+    </>
+  );
+}
+
+export default App;
