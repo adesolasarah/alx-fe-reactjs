7c06aa0ef5eadd63c96f695dfd893488be28f03d
theadesolasarah <williamssarahadesola@gmail.com>
2025-09-30 13:51:13 +0100

    test

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,12 @@
+import TestComponent from "./components/test";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to the Recipe Sharing Platform</h1>
+      <TestComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ recipe-sharing-platform/src/components/test.jsx
@@ -0,0 +1,4 @@
+function TestComponent() {
+  return <div className="p-4 bg-gray-100 rounded">Hello, Tailwind CSS!</div>;
+}
+export default TestComponent;
