58e514d1cdf6def83327575b3b33244d24dee473
theadesolasarah <williamssarahadesola@gmail.com>
2025-09-30 17:18:32 +0100

    Initial setup with React and Tailwind

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    // "./index.html",                 // Vite entry (keeps your setup working)
-    // "./public/index.html",          // Added for the checker
-    "./src/**/*.{js,ts,jsx,tsx}",   // All React files
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
     extend: {},
