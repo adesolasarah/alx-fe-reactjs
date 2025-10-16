21a2806fef51cd79ca373ec1b0e2c479e62c79dc
theadesolasarah <williamssarahadesola@gmail.com>
2025-10-16 01:24:57 +0100

    Add user registration forms with controlled components and Formik

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,13 @@
+import FormikForm from "./components/formikForm";
+import RegistrationForm from "./components/RegistrationForm";
+
+function App() {
+  return (
+    <div>
+      <RegistrationForm/>
+      <FormikForm/>
+    </div>
+  )
+}
+
+export default App;
