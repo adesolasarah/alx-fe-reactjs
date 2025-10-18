88f3313a0303cd63a8e929ae6906d59d4ff8fc63
theadesolasarah <williamssarahadesola@gmail.com>
2025-10-18 12:40:14 +0100

    made a fix

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,13 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import FormikForm from "./components/formikForm";
 import RegistrationForm from "./components/RegistrationForm";
+import PostsComponents from "../../react-query-demo/src/PostsComponent";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
-      <RegistrationForm/>
-      <FormikForm/>
-    </div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <RegistrationForm />
+        <FormikForm />
+        <PostsComponents />
+      </div>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
